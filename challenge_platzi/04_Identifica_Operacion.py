e615510cf6252e494531812a032d73d0f9790081
Fersh5 <dev_fer5@outlook.com>
2024-07-23 17:33:21 -0600

    Ejercicios 4 y 5

--- challenge_platzi/04_Identifica_Operacion.py
+++ challenge_platzi/04_Identifica_Operacion.py
@@ -25,3 +25,43 @@ def op_oculta(num):
 
 for n in range (3,10):
     print(f'{n} = {op_oculta(n)}')
+
+## Bonus
+'''
+Vamos a tomar como base los números del 3 al 8 para multiplciarlos por un mismo factor desconocido (x) que nos da ciertos resultados.
+
+Entonces, dada una función que recibe un array númerico con los resultados debes encontrar el factor multiplicador (x) para obtener ese resultado multiplicando por los números del 3 al 8.
+
+* Si si un solo factor multiplicador difiere del resto se regresa false.
+
+* En el siguiente ejemplo la función regresaria false porque hay una resultado que tiene como factor multiplicador el 6 en lugar del 9 como el resto.
+
+* 3 * x =  27
+* 4 * x =  36
+* 5 * x =  45
+* 6 * x =  54
+* 7 * 6 =  42 <- 👈
+* 8 * x =  72
+
+Input:
+```
+solution([27, 36, 45, 54, 63, 72])
+solution([27, 36, 45, 54, 42, 72])
+```
+Output:
+```
+9
+false
+```
+'''
+lista1 = [27,36,45,54,63,72]
+lista2 = [27,36,45,54,42,72]
+def solution(lista):
+    fact_v = list(range(3,9))
+    result = [lista[i]/fact_v[i] for i in range(0,len(lista))]
+    if len(set(result)) == 1:
+        return set(result).pop()
+    return False
+
+print(solution(lista1))
+print(solution(lista2))
---
+++ challenge_platzi/05_Signos_Matematicos.py
@@ -0,0 +1,24 @@
+# 05. **Signos Matematicos**
+'''
+Encuentra el resultado de la siguiente operación matemática:
+4 * 3 + 7 * 5 - 55 ÷ 11 + 9
+'''
+## Desarrollo
+'''
+Para resolver este ejercicio solo hay que seguir el orden de la jerarquia de operaciones:
+1. Signos de agrupacion {}[]()
+2. Exponentes y raices √ ^
+3. Multiplicacion y divicion * /
+4. Sumas y Restas 
+'''
+## Solucion
+'''
+4 * 3 + 7 * 5 - 55 ÷ 11 + 9
+(4*3) + (7*5) - (5) + 9
+ 12   +  35   -  5  + 9
+= 51
+'''
+
+#Comprobamos el resultado
+r= 4 * 3 + 7 * 5 - 55 / 11 + 9
+print(r)
