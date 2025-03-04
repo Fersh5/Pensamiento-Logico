66f9895ea931cf30a387cc23e4e9946b61d17847
Fersh5 <dev_fer5@outlook.com>
2025-03-03 23:56:12 -0600

    day 3

--- 7_days_of_code_JS/day_3.js
+++ 7_days_of_code_JS/day_3.js
@@ -11,18 +11,92 @@
  
 ! Lo importante es que la persona que esté jugando siempre pueda elegir qué decisión tomar para aprender y desarrollarse en el área de programación.
 
-* *Además, también es esencial que, al final del juego, pueda ingresar tantas tecnologías como desee en la lista de aprendizaje. */
+* *Además, también es esencial que, al final del juego, pueda ingresar tantas tecnologías como desee en la lista de aprendizaje. 
+*/
 
-let areas ={'FrontEnd':[],'Backend':[]}
-let area = prompt('Que deseas aprender (elige con "1" o "2"): \n 1. FrontEnd \n 2. BackEnd');
+let areas = {'Front-End':['React','Vue'],'Back-End':['C#','Java']};
+let selected = {};
 
-let enfoque = prompt(`¿Quieres especializarte mas en ${areas[area+1]} (Opcion: 1) ? \no ¿Deseas ser Fullstack? (Opcion: 2)\n`)
-if (area+1 == 1){
-    area['FrontEnd']+=[]
-    
+function select (selected){
+    let area = '';
+    let opc;
+    do {
+        opc =parseInt(prompt('Que deseas aprender (elige con 1 o 2): \n 1. Front-End \n 2. Back-End'));
+        console.log(typeof opc, '\n',opc)
+        if (opc == 1){
+            area='Front-End';
+            selected[area]=[];
+            return selected;
+        } else if (opc == 2){
+            area='Back-End';
+            selected[area]=[];
+            return selected;
+        } else{
+            alert('Dato invalido, por favor ingresa 1 o 2');    
+        }
+    } while(opc != 1 && opc != 2)
 }
 
-let rea = {
-    s:5
-
+function areaEsp(userSelection){
+    let framework_language=0;
+    do{
+        if ('Front-End' in userSelection){
+            framework_language = parseInt(prompt('Que framework deseas utilizar:\n1. React \n2. Vue'));
+            if (framework_language == 1){
+                userSelection['Front-End'].push('React');
+                return userSelection;
+            } else if (framework_language == 2){
+                userSelection['Front-End'].push('Vue');
+                return userSelection;
+            }
+        }else if ('Back-End' in  userSelection){
+            framework_language = parseInt(prompt('Que lenguaje deseas utilizar:\n1. C# \n2. Java'));
+            if (framework_language == 1){
+                userSelection['Back-End'].push('C#')
+                return userSelection;
+            } else if (framework_language == 2){
+                userSelection['Back-End'].push('Java')
+                return userSelection;
+            } 
+        }else{
+            alert('Dato invalido, por favor ingresa 1 o 2');
+        }
+    }while (framework_language!=1 && framework_language!=2)
 }
+
+function ultimaEleccion(userSelection) {
+    let enfoque;
+    let area;
+    do{
+        enfoque = parseInt(prompt(`¿Quieres?: \n1. ¿Especializarte mas en ${'Front-End' in userSelection ? 'Front-End':'Back-End'}? \n2. ¿Deseas ser Fullstack?\n`));
+        if (enfoque == 1){
+            area = 'Front-End' in userSelection ? 'Front-End' : 'Back-End';
+            return [area,userSelection]
+        }else if (enfoque == 2){
+            area = 'Front-End' in userSelection ? 'Back-End':'Front-End';
+            userSelection[area]=[];
+            return [area,userSelection] 
+        }else{
+            alert('Dato invalido, por favor ingresa 1 o 2');
+        }
+    }while(enfoque != 1 && enfoque != 2)
+}
+
+function tecAdd(seleccion,area){
+    let addArea = 1
+    while(addArea != 0){
+        addArea = prompt('Escribe el nombre de las tecnologias que deseas profundizar: \n(Para salir ingresa 0)\n');
+        addArea == 0 ? 0 : seleccion[area].push(addArea);
+    }
+    return seleccion;
+}
+
+let area;
+let userSelection = select(selected);
+userSelection = areaEsp(userSelection);
+[area,userSelection] = ultimaEleccion(userSelection);
+userSelection = tecAdd(userSelection,area);
+
+let parrafo = document.getElementById('tec');
+parrafo.innerHTML = JSON.stringify(userSelection,null,2);
+console.log(userSelection);
